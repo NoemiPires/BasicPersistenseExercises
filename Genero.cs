03a130784b9edf874f657316ba5bb6bb3aca126c
guoliveira0 <luizgustavo160206@gmail.com>
2025-09-09 11:17:55 -0300

    Primeiro commit

---
+++ Genero.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BasicPersistenceExercises
+{
+    public enum Genero
+    {
+        MASCULINO, FEMININO, NAO_DECLARADO
+    }
+}
---
+++ Paciente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPersistenceExercises
+{
+    public class Paciente
+    {
+        [Required]
+        [MaxLength(45)]
+        public String? Nome {  get; set; }
+
+
+        private DateOnly _nascimento;
+        public DateOnly Nascimento 
+        {
+            get => _nascimento;
+            set
+            {
+                if (_nascimento > DateOnly.FromDateTime(DateTime.Now))
+                {
+                    throw new Exception("Data de Nascimento não pode ser futura");
+                }
+                _nascimento = value;
+            } 
+        }
+        public Genero Genero { get; set; }
+        public Boolean Condicao {  get; set; }
+        public DateOnly UltimaConsulta {  get; set; }
+    }
+}
